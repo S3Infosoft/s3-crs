9122802e6c498c23f456f77e397c22e5e864ce94
abhinavsri360 <abhinav.022000@gmail.com>
2019-08-11 06:57:04 +0530

    styling of login and signup
    All CSS and JS files are included

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,21 @@
 <template>
   <div id="app">
+    <Sidebar />
     <router-view></router-view>
   </div>
 </template>
 
 <script>
-
+import Sidebar from './components/Sidebar'
 export default {
-  name: 'app'
+  name: 'app',
+  components:{ 
+    Sidebar
+  }
 }
 </script>
+<style>
+  router-view{
+    float: right;
+  }
+</style>
---
+++ src/components/Sidebar.vue
@@ -0,0 +1,51 @@
+<template>
+  <!-- Sidebar -->
+    <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+
+      <!-- Sidebar - Brand -->
+      <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+        <div class="sidebar-brand-icon rotate-n-15">
+          <i class="fas fa-laugh-wink"></i>
+        </div>
+        <div class="sidebar-brand-text mx-3">
+          <router-link class="sidebar-brand d-flex align-items-center justify-content-center" to="/">CRS</router-link>
+        </div>
+      </a>
+
+      <!-- Heading -->
+      <div class="sidebar-heading">
+        Welcome to CRS
+      </div>
+
+      <!-- Divider -->
+      <hr class="sidebar-divider my-0">
+
+      <!-- Nav Item - Charts -->
+      <li class="nav-item">
+        <a class="nav-link" href="Login.vue">
+          <router-link class="nav-link" to="/login">Login</router-link></a>
+      </li>
+
+      <!-- Nav Item - Tables -->
+      <li class="nav-item">
+        <a class="nav-link" href="Signup.vue">
+          <router-link class="nav-link" to="/signup">Signup</router-link></a>
+      </li>
+
+      <!-- Divider -->
+      <hr class="sidebar-divider d-none d-md-block">
+
+      <!-- Sidebar Toggler (Sidebar) -->
+      <div class="text-center d-none d-md-inline">
+        <button class="rounded-circle border-0" id="sidebarToggle"></button>
+      </div>
+
+    </ul>
+    <!-- End of Sidebar -->
+</template>
+
+<script>
+export default {
+  
+}
+</script>
